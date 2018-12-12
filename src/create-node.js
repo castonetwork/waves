f91e441562b585fc89d5702b09e913264b37b188
JaehoLee <spectrick@gmail.com>
2018-12-12 15:57:09 +0900

    change websocket-star addresses to the default public websocket-star

--- src/create-node.js
+++ src/create-node.js
@@ -4,9 +4,9 @@ const multiaddr = require('multiaddr');
 const createNode = async ()=> new Promise((resolve, reject) => {
   PeerInfo.create((err, peerInfo) => {
     if (err) reject(err);
-    // peerInfo.multiaddrs.add(multiaddr("/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star/"));
-    peerInfo.multiaddrs.add(multiaddr("/ip4/64.137.231.84/tcp/9090/ws/p2p-websocket-star/"));
-    peerInfo.multiaddrs.add(multiaddr("/ip4/59.10.206.150/tcp/9999/ws/p2p-websocket-star/"));
+    peerInfo.multiaddrs.add(multiaddr("/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star/"));
+    // peerInfo.multiaddrs.add(multiaddr("/ip4/64.137.231.84/tcp/9090/wss/p2p-websocket-star/"));
+    // peerInfo.multiaddrs.add(multiaddr("/ip4/59.10.206.150/tcp/9999/wss/p2p-websocket-star/"));
     const node = new Node({peerInfo});
     resolve(node);
   });
