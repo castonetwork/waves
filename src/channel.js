59dfa426a7ca6dc757791c8559fcc3a93282384c
JaehoLee <spectrick@gmail.com>
2018-12-17 15:53:26 +0900

    impl: add loading animation screen

--- src/channel.js
+++ src/channel.js
@@ -40,7 +40,7 @@ const updateChannelElement = (peerId, info) =>{
     }
     item.querySelector(".channelInfo > .viewer").textContent = "0";
     document.querySelector("dl").appendChild(item);
-    document.querySelector("dd.noItem") && document.querySelector("dd.noItem").remove();
+    document.body.setAttribute("data-scene", "list");
   } else {
     /* update info */
 
@@ -112,6 +112,86 @@ const processEvents = async event => {
   }
 };
 
+const initLoadingScreen = ()=> {
+  var svgEl = document.querySelector('.animated-lines');
+
+  var randomRange = function(min, max) {
+    return ~~(Math.random() * (max - min + 1)) + min
+  };
+
+  var numberOfLines = 20,
+    lineDataArr = [];
+
+  var createPathString = function() {
+
+    var completedPath = '',
+      comma = ',',
+      ampl = 50; // pixel range from 0, aka how deeply they bend
+
+    for (var i = 0; i < numberOfLines; i++) {
+
+      var path = lineDataArr[i];
+
+      var current = {
+        x: ampl * Math.sin(path.counter / path.sin),
+        y: ampl * Math.cos(path.counter / path.cos)
+      };
+
+      var newPathSection = `M${path.startX}${comma}${path.startY} 
+        Q${path.pointX}${comma}${(current.y * 1.5).toFixed(3)}
+        ${((current.x) / 10 + path.centerX).toFixed(3)}${comma}${((current.y) / 5 + path.centerY).toFixed(3)}
+        T${path.endX}${comma}${path.endY}`;
+      path.counter++;
+
+      completedPath += newPathSection;
+
+    };
+
+    return completedPath;
+
+  };
+
+  var createLines = function() {
+
+    var newPathEl = document.createElementNS('http://www.w3.org/2000/svg', 'path'),
+      // higher is slower
+      minSpeed = 85,
+      maxSpeed = 150;
+
+    // create an arr which contains objects for all lines
+    // createPathString() will use this array
+    for (var i = 0; i < numberOfLines; i++) {
+
+      var lineDataObj = {
+        counter: randomRange(1, 500), // a broad counter range ensures lines start at different cycles (will look more random)
+        startX: randomRange(-5, -40),
+        startY: randomRange(-5, -30),
+        endX: randomRange(200, 220), // viewbox = 200
+        endY: randomRange(120, 140), // viewbox = 120
+        sin: randomRange(minSpeed, maxSpeed),
+        cos: randomRange(minSpeed, maxSpeed),
+        pointX: randomRange(30, 55),
+        centerX: randomRange(90, 120),
+        centerY: randomRange(60, 70)
+      }
+
+      lineDataArr.push(lineDataObj)
+
+    }
+
+    var animLoop = function() {
+      newPathEl.setAttribute('d', createPathString());
+      requestAnimationFrame(animLoop);
+    }
+
+    // once the path elements are created, start the animation loop
+    svgEl.appendChild(newPathEl);
+    animLoop();
+
+  };
+
+  createLines();
+};
 const initApp = async () => {
   let streamers = {};
   console.log("init app");
@@ -122,6 +202,9 @@ const initApp = async () => {
   listDOM = document.querySelector('dd.item');
   channelItem = listDOM.cloneNode(true);
   listDOM.remove();
+
+  document.body.setAttribute('data-scene', 'noItem')
+  initLoadingScreen();
 
   const node = await createNode();
   node.on("peer:discovery", peerInfo => {
@@ -164,10 +247,7 @@ const initApp = async () => {
     const element = document.getElementById(id);
     if (element) {
       if (document.querySelector("dl").children.length===1) {
-        const item = document.createElement('dd');
-        item.setAttribute("class", "noItem");
-        item.textContent = "No channels";
-        document.querySelector("dl").appendChild(item);
+        document.body.setAttribute("data-scene", "noItem");
       }
       element.remove();
     }
